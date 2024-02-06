7e12b2c8e00c7a9e571c9eb2222ba80d8d8ae800
Matthew Christian <matthewchristian33@gmail.com>
2024-02-05 19:10:12 -0500

    Finished Week 3

--- week2.cpp
+++ week2.cpp
@@ -1,128 +1,22 @@
-/*
-  Initial Class.
-class MyFirstClass {
+/**
+ * Assignment: Lab 3
+ * Course: CST819: C++ Programming
+ * Lab Section: 303
+ * Lab Professor: Mohammad Patoary
+ * Due Date: 01/30/2024
+ * Submission Date: 01/30/2024
 */
-
-/*
-  This is an access specifier, any variable or
-  function following it now have this level of access.
- private:
-*/	
-	/*
-	  These are Class variables/members.
-	int age;
-	char* firstName;
-	char* lastName;
-	*/
-
-/*
-  This is also an access specifier.
- public:
- */
-	/*
-	  Default Constructor without Constructor Chaining.
-	MyFirstClass() {
-		age = 0;
-		firstName = NULL;
-		lastName = NULL;
-	}
-	*/
-
-	/*
-	  Parameterized Constructor.
-	MyFirstClass(int a, char* f, char* l) : firstName(f), lastName(l) {
-		age = a;
-	 }
-	*/
-
-	/*
-	  Default Constructor using Constructor Chaining.
-	MyFirstClass() : MyFirstClass(0, NULL, NULL) {}
-	*/
-
-	/*
-	  This is a Destructor.
-	  Destructors do not have parameters.
-	  There can only be one destructor.
-	  The compiler calls the destructor for you.
-	  Never call a destructor directly.
-	~MyFirstClass() {}
-	*/
-
-	/*
-	  This is NOT a Constructor, NOR does it call a
-	  Constructor.
-	  This is a Function declaration.
-	  The Function's name is mfc, and it returns an Object
-	  of MyFirstClass{}.
-	MyFirstClass mfc();
-	*/
-
-	/*
-	  Returns the variable age.
-	int getAge() {
-		return age;
-	}
-	*/
-
-	/*
-	  Returns the variable firstName.
-	char* getFirstName() {
-		return firstName;
-	}
-	*/
-
-	/*
-	  Returns the variable lastName.
-	char* getLastName() {
-		return lastName;
-	}
-	*/
-
-/*
-  This is also an access specifier.
- protected:
-*/
-	/*
-	  Sets the value of the variable age.
-	void setAge(int a) {
-		age = a;
-	}
-	*/
-
-	/*
-	  Sets the value of the variable firstName.
-	void setFirstName(char* f) {
-		firstName = f;
-	}
-	*/
-
-	/*
-	  Sets the value of the variable lastName.
-	void setLastName(char* l) {
-		lastName = l;
-	}
-};
-	*/
-
-
 #include<iostream>
+#include<string>
 using namespace std;
 
-/*
-  Namespaces allow you to wrap your Class names so that
-  they won't conflict with other Class names.
-  It is good practice to create a namespace so that
-  your Class names are unique.
-  Namespaces are similar to packages in Java
-  The following statement creates a namespace.
-*/
 namespace CST8219 {
 	// Class to represent a vehicle.
 	class Vehicle {
 	 private:
 		int numWheels;
 		int numDoors;
+		Vehicle* pVehicle = NULL;
 
 	 public: 
 		Vehicle(int w, int d) {
@@ -144,52 +38,71 @@ namespace CST8219 {
 		~Vehicle() {
 			cout << "In destructor" << endl;
 		}
+
+		void userVehicle() {
+			int userD = 0;
+			int userW = 0;
+			char userChar = NULL;
+
+			while (userChar != 'q') {
+				userChar = NULL;
+				userD = 0;
+				userW = 0;
+				cout << "Press ANY KEY to create a vehicle or q to quit: ";
+				cin >> userChar;
+				if (userChar != 'q') {
+					delete pVehicle;
+					// Asks user for input.
+					cout << "Enter number of doors: ";
+					while ((!(cin >> userD)) || (userD <= 0)) {
+						cout << "Error! Please enter a number: ";
+						cin.clear();
+						cin.ignore(256, '\n');
+					}
+
+					// Asks user for input.
+					cin.clear();
+					cin.ignore(256, '\n');
+					cout << "Enter number of wheels: ";
+					while ((!(cin >> userW)) || (userW <= 0)) {
+						cout << "Error! Please enter a number:";
+						cin.clear();
+						cin.ignore(256, '\n');
+					}
+
+					pVehicle = new Vehicle(userW, userD);
+				} else {
+					continue;
+				}
+			}
+		}
 	};
 }
 
 using namespace CST8219;
 int main(int argc, char** argv) {
-	/*
-	  Calls an empty Constructor.
-	MyFirstClass mfc;
-	*/
-
-	/*
-	  Also calls an empty Constructor.
-	MyFirstClass mfc = MyFirstClass();
-	*/
-
-	/*
-	  Calls a Constructor with parameters.
-	MyFirstClass mfc(15, "Joshua", "Dane");
-	*/
-
-	/*
-	  Also calls a Constructor with parameters.
-	MyFirstClass mfc = MyFirstClass(5, "Ally", "Johnson");
-	*/
-
-	/*
-	  Outputs a message to the screen.
-	cout << "Hello World!" << endl;
-	*/
-
-	/*
-	  Calls the Constructor Vehicle().
-	CST8219::Vehicle myVehicle;
-	*/
-
-	/*
-	  Calls the Constructor Vehicle(int).
-	CST8219::Vehicle myVehicle(4);
-	*/
-	
-	/*
-	  Calls the Constructor Vehicle(int, int).
-	*/
-	CST8219::Vehicle myVehicle(4, 2);
+	// Calls the Constructor Vehicle().
+	Vehicle vehicle1;
 
 	// Outputs a message to the screen.
-	cout << "I made a Vehicle!" << endl;
+	cout << "Vehicle takes " << sizeof(vehicle1) << endl;
+
+	// Calls the Constructor Vehicle(int).
+	Vehicle vehicle2(4);
+
+	// Outputs a message to the screen.
+	cout << "Vehicle takes " << sizeof(vehicle2) << endl;
+
+	// Calls the Constructor Vehicle(int, int).
+	Vehicle vehicle3(4, 2);
+
+	// Outputs a message to the screen.
+	cout << "Vehicle takes " << sizeof(vehicle3) << endl;
+
+	// Outputs a message to the screen.
+	cout << "I made Vehicles!" << endl;
+
+	vehicle1.userVehicle();
+
 	return 0;
 }
